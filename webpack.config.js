1198f0245799ca4801a13d34147eb1c92144736b
Mu Ye <emailyemu@gmail.com>
2023-01-04 18:44:35 -0800

    Project scaffold

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: './src/index.ts',
+  output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+};
