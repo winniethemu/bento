e7f54c6b9e7494098d9ef706a305a73d1c661823
Mu Ye <emailyemu@gmail.com>
2023-01-19 19:27:00 -0800

    Keyboard a11y attempt

--- src/components/Select/NativeSelect.tsx
+++ src/components/Select/NativeSelect.tsx
@@ -34,7 +34,7 @@ const Menu = styled.ol<Pick<Props, 'open'>>`
 
 const MenuItem = styled.li<Props>`
   border-radius: 6px;
-  font-size: 14px;
+  font-size: 16px;
   font-weight: ${props => props.selected ? 'bold' : 'normal'};
   line-height: 32px;
   min-width: 200px;
@@ -56,6 +56,8 @@ const MenuIcon = styled.span`
   color: ${colors.theme.radiant.coral};
 `;
 
+const validKeys = [' ', 'ArrowDown', 'ArrowUp', 'Enter', 'Escape'];
+
 export const NativeSelect = (props: SelectProps) => {
   const { options, name } = props;
   const [open, setOpen] = useState(false);
@@ -69,15 +71,41 @@ export const NativeSelect = (props: SelectProps) => {
     setValue(val);
   }
 
+  const handleKeyOpen = (event: React.KeyboardEvent) => {
+    if (validKeys.indexOf(event.key) < 0) return;
+    if (event.key === 'Escape') return;
+    if (!open) {
+      //TODO: set focus
+      setOpen(true);
+    }
+  };
+
+  const handleKeySelect = (event: React.KeyboardEvent, index: number) => {
+    if (!open || validKeys.indexOf(event.key) < 0) return;
+
+    switch(event.key) {
+      case 'ArrowUp':
+        break;
+      case 'ArrowDown':
+        break;
+      case ' ':
+      case 'Enter':
+        setValue(options[index].value);
+      default:
+        setOpen(false);
+    }
+  };
+
   return (
     <Wrapper onClick={handleToggle}>
-      <Menu open={open}>
-        {options.map((option) => {
+      <Menu open={open} tabIndex={0} onKeyDown={handleKeyOpen}>
+        {options.map((option, index) => {
           return (open || option.value === value) ?
             <MenuItem
               key={option.value as string}
               open={open}
               onClick={() => handleSelect(option.value)}
+              onKeyDown={(e) => handleKeySelect(e, index)}
               selected={option.value === value}
             >
               {option.label}
